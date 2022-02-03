2afa85c78a87ef85d7743d2ea16ed609e99e29e6
Ian Toltz <itoltz@gmail.com>
2022-02-03 16:33:32 -0500

    Preloading images

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,15 @@
 import expansionCore from "../data/core.js";
 import { generate, pick, display } from "./util.js";
 
+[
+	...expansionCore.heroes,
+	...expansionCore.villains,
+	...expansionCore.environments,
+].forEach(o => {
+	const preload = new Image();
+	preload.src = o.image;
+});
+
 document.getElementById("generate-button").addEventListener("click", () => generate({
 	heroes: expansionCore.heroes,
 	villains: expansionCore.villains,
