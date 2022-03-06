2197a13c9c8fe20adedf38a40234cbe2ad614cb9
Ian Toltz <itoltz@gmail.com>
2022-03-06 15:42:20 -0500

    Improvements

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,13 @@
 import { generate } from "./util.js";
 
+const infoPanel = document.querySelector(".info");
 document.getElementById("generate-button").addEventListener("click", generate);
+infoPanel.querySelector(".info-target").addEventListener("click", () => {
+	infoPanel.classList.add("info__expanded");
+});
+infoPanel.querySelector(".info-close").addEventListener("click", () => {
+	infoPanel.classList.remove("info__expanded");
+});
+
 
 generate();
